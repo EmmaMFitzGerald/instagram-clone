122b15788e10c373e96e7088c1fafb2607f92ed2
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-04-01 17:03:09 -0400

    added first mocha test

--- src/test/live/example.test.ts
+++ src/test/live/example.test.ts
@@ -1,5 +1,9 @@
 import { describe, it } from "mocha";
 import { expect } from "chai";
+import { table } from "console";
+var AWS = require("aws-sdk");
+
+
 
 describe("Example Test Series", () => {
     it("should return an expected value", () => {
@@ -9,4 +13,26 @@ describe("Example Test Series", () => {
 
         expect(target).to.equal(11);
     });
+
+    it("should create item in the database", function(done){ 
+        
+        var params = {
+            Item: {
+                "PhotoID": "123566"
+            },
+            TableName: "photo_table"
+        };
+
+
+          var documentClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+          documentClient.put(params, function(_err: any, data: any) {
+            expect(_err).length.to.equal(null)
+            done();
+          });
+        });
+
+        
+    
 });
+
+
